95323c7744ceea2d802320262974f0d5da5c68f8
unknown <puia_victor@yahoo.com>
2022-01-06 23:14:46 +0200

    implemented buttons in main activity and in the scan item activity

--- app/src/main/java/com/example/foodidentifier/MainActivity.java
+++ app/src/main/java/com/example/foodidentifier/MainActivity.java
@@ -3,14 +3,36 @@ package com.example.foodidentifier;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan_item);
+        setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        Button scanItemActivity = (Button) findViewById(R.id.scan_item);
+        Button viewPreviousScansActivity = (Button) findViewById(R.id.previous_scans);
+        scanItemActivity.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity.this, ScanItemActivity.class));
+                    }
+                }
+        );
+        viewPreviousScansActivity.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity.this, ScanItemActivity.class));
+                    }
+                }
+        );
     }
 }
